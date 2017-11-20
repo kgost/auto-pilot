312078bffc7fcbba5177d94b31a9979ac19be9e5
John Bland <johnbone23@gmail.com>
2017-11-20 15:36:06 -0500

    move stream out of function

--- filter-tweets.js
+++ filter-tweets.js
@@ -19,17 +19,87 @@ DefaultUser.findOne( {}, function( err, defaults ) {
 			throw err;
 		}
 
-		async.map( users, function( user, callback ) {
-			callback( null, startStream( user, defaults._id ) );
-		}, function( err, results ) {
-			if ( err ) {
-				console.log( err );
-			}
+		let user = users[0];
+		let defaultsId = defaults._id;
 
-			for ( let i = 0; i < results.length; i++ ) {
-				console.log( results[i] );
+		const client = new Twitter({
+			consumer_key: user.cKey,
+			consumer_secret: user.cSecret,
+			access_token_key: user.aKey,
+			access_token_secret: user.aSecret,
+		});
+
+		let track = '';
+
+		for ( let i = 0; i < user.filterTerms.length; i++ ) {
+			track += user.filterTerms[i];
+
+			if ( i != user.filterTerms.length - 1 ) {
+				track += ',';
 			}
+		}
+
+		let stream = client.stream( 'statuses/filter?tweet_mode=extended', { track: track } );
+
+		stream.on( 'data', function( event ) {
+			console.log( event );
+
+			User.findById( user._id, function( err, user ) {
+				if ( err ) {
+					console.log( err );
+				}
+
+				DefaultUser.findById( defaultsId, function( err, defaults ) {
+					if ( err ) {
+						console.log( err );
+					}
+						
+					if ( event.user && !event.retweeted_status && !event.possibly_sensitive 
+						&& event.user.followers_count >= 50000 && event.lang == 'en' 
+						&& !matchId( defaults.bannedUserIds, event.user.id ) 
+						&& !matchId( user.bannedUserIds, event.user.id ) 
+						&& !matchId( user.coolingUserIds, event.user.id )
+						&& !matchWord( defaults.bannedWords, event.full_text )
+						&& !matchWord( user.bannedWords, event.full_text ) ) {
+							let following = ( event.user.following == null ) ? false : true;
+							let start = 0;
+							let end = user.potentialRTs.length - 1;
+
+							while ( start <= end ) {
+								let mid = parseInt( ( end + start ) / 2 );
+
+								if ( user.potentialRTs[mid].followers == event.user.followers_count ) {
+									break;
+								} else if ( user.potentialRTs[mid].followers < event.user.followers_count ) {
+									start = mid + 1;
+								} else if ( user.potentialRTs[mid].followers > event.user.followers_count ) {
+									end = mid - 1;
+								}
+							}
+
+							user.potentialRTs.splice( start, 0, { id: event.id, userId: event.user.id, followers: event.user.followers_count, followingUser: following } );
+							console.log( user );
+							user.save();
+					}
+				} );
+			} );
 		} );
+
+		stream.on( 'error', function( err ) {
+			console.log( err );
+		} );
+
+		// async.map( users, function( user, callback ) {
+		// 	callback( null, startStream( user, defaults._id ) );
+		// }, function( err, results ) {
+		// 	if ( err ) {
+		// 		console.log( err );
+		// 	}
+
+		// 	for ( let i = 0; i < results.length; i++ ) {
+		// 		console.log( results[i] );
+		// 	}
+		// } );
 
 		// for ( let i = 0; i < users.length; i++ ) {
 		// 	streams.push( startStream( users[i], defaults._id ) );
