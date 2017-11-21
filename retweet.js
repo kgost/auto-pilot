6ab89adbe72cee36e18851add431114a5076fd28
John Bland <johnbone23@gmail.com>
2017-11-20 21:19:15 -0500

    fix retweet

--- retweet.js
+++ retweet.js
@@ -33,13 +33,12 @@ User.find( {}, function( err, users ) {
 
 		let k = Math.floor( Math.random() * retweets.length );
 		client.post('statuses/retweet/' + user.potentialRTs[k].id, function(error, tweet, response) {
-			console.log( tweet );
 			let recentUser = false;
 			let start = 0;
 			let end = user.usersTwoDays.length - 1;
 
 			while ( start <= end ) {
-				let mid = ( int ) ( ( end + start ) / 2 );
+				let mid = parseInt( ( end + start ) / 2 );
 
 				if ( user.usersTwoDays[mid].id == user.potentialRTs[k].id ) {
 					user.usersTwoDays[mid].count += 1;
